eaa542ab93fa1b9d4ce31ef1de1630e2a34bf7b3
GCZ <973133378@qq.com>
2016-11-23 00:28:10 +0800

    1

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-	validates :userName, presence: true, length:{maximum: 20}
-	EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length:{maximum: 50}, format: {with: EMAIL} ,uniqueness: { case_sensitive: false } 
-
-	:has_secure_password
-	validates :password, length:{minimum: 6}
-
-end
